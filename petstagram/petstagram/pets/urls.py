46f627b1d2e91a14637f6841d2e3619f671cd9ec
tidorino <tidorino@hotmail.com>
2022-10-01 15:27:24 +0300

    Added files for Petstagram workshop part 1

--- petstagram/petstagram/pets/urls.py
+++ petstagram/petstagram/pets/urls.py
@@ -1,1 +1,12 @@
-urlpatterns = ()
+from django.urls import path, include
+
+from petstagram.pets.views import add_pet, delete_pet, pet_details, edit_pet
+
+urlpatterns = (
+    path('add/', add_pet, name='add pet'),
+    path('<str:username>/pet/<slug:pet_name>/', include([
+        path('', pet_details, name='pet details'),
+        path('edit/', edit_pet, name='edit pet'),
+        path('delete/', delete_pet, name='delete pet'),
+    ])),
+)
--- petstagram/petstagram/photos/urls.py
+++ petstagram/petstagram/photos/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = ()
+from django.urls import path, include
+
+from petstagram.photos.views import add_photo, photo_details, edit_photo
+
+urlpatterns = (
+    path('add/', add_photo, name='add photo'),
+    path('<int:pk>/', include([
+        path('', photo_details, name='photo details'),
+        path('edit/', edit_photo, name='edit photo'),
+    ])),
+)
