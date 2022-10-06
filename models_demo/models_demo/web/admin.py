b16f4a8255db613f801b49b6177036d1cc248a59
tidorino <tidorino@hotmail.com>
2022-10-06 14:05:34 +0300

    new update

--- models_demo/models_demo/web/admin.py
+++ models_demo/models_demo/web/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from models_demo.web.models import Employee
+from models_demo.web.models import Employee, Department, Project
 
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
     pass
--- models_demo/models_demo/web/models.py
+++ models_demo/models_demo/web/models.py
@@ -1,8 +1,24 @@
 from django.db import models
-from django.utils import timezone
 
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f'Id: {self.pk}; Name: {self.name}'
+
+
+class Project(models.Model):
+    name = models.CharField(
+        max_length=30,
+    )
+    code_name = models.CharField(
+        max_length=10,
+        unique=True,
+    )
+    deadline = models.DateField()
+
+
 class Employee(models.Model):
     first_name = models.CharField(
         max_length=30
@@ -14,9 +30,70 @@ class Employee(models.Model):
     age = models.IntegerField()
 
     email = models.EmailField(
+        'EMAIL',  # verbose_name = 'EMAIL'
         unique=True,
         max_length=200,
-        # null=True
+
+    )
+
+    works_full_time = models.BooleanField(
+        default=True,
+        blank=True,
+        null=True,
+    )
+    my_level = models.CharField(
+        max_length=3,
+        choices=(
+            ('jr', ' junior'),
+            ('reg', 'regular'),
+            ('sr', 'senior'),
+        ),
+        null=True,
+    )
+    job_level = models.CharField(
+        max_length=30,
+        default='junior',
+    )
+
+    business_account = models.CharField(
+        max_length=30,
+        unique=True,
+        default='unknown',
+    )
+
+    second_email_address = models.EmailField(blank=True)
+    birth_date = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+        default=1
+    )
+
+    projects = models.ManyToManyField(
+        Project,
+        related_name='employees',
+    )
+
+
+class AccessCard(models.Model):
+    employee = models.OneToOneField(
+        Employee,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+class EmployeesProjects(models.Model):
+    employee_id = models.ForeignKey(
+        Employee,
+        on_delete=models.RESTRICT,
+    )
+    project_id = models.ForeignKey(
+        Project,
+        on_delete=models.RESTRICT,
     )
 
     @property
