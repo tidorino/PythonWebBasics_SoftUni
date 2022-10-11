6c4979802ab2730f4799116b7f2d6a98c31481e4
tidorino <tidorino@hotmail.com>
2022-10-11 12:06:43 +0300

    new update

--- petstagram/petstagram/pets/models.py
+++ petstagram/petstagram/pets/models.py
@@ -1,3 +1,37 @@
+from django.core.validators import URLValidator
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Pet(models.Model):
+    MAX_LENGTH_NAME = 30
+
+    name = models.CharField(
+        max_length=MAX_LENGTH_NAME,
+        null=False,
+        blank=False
+    )
+
+    personal_pet_photo = models.URLField(
+        null=False,
+        blank=False,
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        blank=False,
+    )
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(
+                f'{self.id}-{self.name}'
+            )
+        return super().save(*args, **kwargs)
--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-77p@l3$(=m5(fpw!ax#a^q=b2xjesu_2b5&n&i-&7d%&v)4kf-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -77,16 +80,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'petstagram.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram_db',
+        'USER': 'postgres',
+        'PASSWORD': 'tidorino',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
